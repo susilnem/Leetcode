8917ab38d2e61b5beaf7289b5249473689edc417
Sushil Tiwari <susilsaurav@gmail.com>
2023-02-21 20:43:24 +0545

    Reverse String

---
+++ TopSWE/Reverse String/ReverseString.py
@@ -0,0 +1,46 @@
+
+"""
+
+Write a function that reverses a string.
+The input string is given as an array of characters s.
+You must do this by modifying the input array in -place with O(1) extra memory.
+
+Example 1:
+-------------
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+
+Example 2:
+-------------
+
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+
+"""
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        l,r=0,len(s)-1
+        while l<r :
+           s[l],s[r]=s[r],s[l]
+           l+=1
+           r-=1
+
+
+OR
+
+
+#using slice method Pythonic Way
+
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        s[:] = s[::-1]
+
